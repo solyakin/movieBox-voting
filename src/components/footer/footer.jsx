667696cf9cafd14eed723ca80b04889fe65727ae
solomon Akinlade <48894861+solyakin@users.noreply.github.com>
2021-01-17 15:53:14 +0100

    Add files via upload

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../footer/footer.css';
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <p>code with <span className="heart-icon">&#9825;</span> by Solomon Akinlade</p>
+            <p><span>&#169;</span>2021</p>
+        </div>
+    )
+}
+
+export default Footer;
